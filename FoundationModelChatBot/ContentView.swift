9e39544732066b655626ee2f7df091b404a6dc22
Aaron Butler <aaronkbutler@icloud.com>
2025-06-10 22:55:48 -0400

    Initial Commit

---
+++ FoundationModelChatBot/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FoundationModelChatBot
+//
+//  Created by Aaron Butler on 6/10/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FoundationModelChatBot/FoundationModelChatBotApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoundationModelChatBotApp.swift
+//  FoundationModelChatBot
+//
+//  Created by Aaron Butler on 6/10/25.
+//
+
+import SwiftUI
+
+@main
+struct FoundationModelChatBotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
